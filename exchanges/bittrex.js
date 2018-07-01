a345f6d1cf15f207133f2199cf0038b49c24cd47
JovanovicJovan <jjovanovic24@hotmail.com>
2018-07-01 18:17:29 +0200

    Added kuncoin and bittrex

--- exchanges/bittrex.js
+++ exchanges/bittrex.js
@@ -1,7 +1,29 @@
 const BigNumber = require('bignumber.js');
+const Bittrex = require('bittrex-api-node');
 
 const getUserBalance = async (context) => {
-  return {};
+  if (!context.apiKey || !context.apiSecret) {
+    throw new Error();
+  }
+
+  const client = Bittrex({
+    publicKey: context.apiKey, // d84410ba29114b9281141c1457331702
+    secretKey: context.apiSecret // 384ecd4e2af948b6a6f66c788c9890f8
+  });
+
+  const nonEmptyBalances = {};
+
+  await client.getBalances().then((response) => {
+    response.result.forEach(element => {
+      const BigNum = new BigNumber(element.Balance.Balance);
+
+      if (BigNum.gt(new BigNumber(0))) {
+        nonEmptyBalances[element.Currency.Currency] = BigNum;
+      }
+    });
+  }).catch(console.error);
+
+  return nonEmptyBalances;
 };
 
 const getUserBalanceDummy = async (context) => {
---
+++ exchanges/kucoin.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Kucoin = require('kucoin-api');
+const BigNumber = require('bignumber.js');
+
+const getUserBalance = async (context) => {
+  // if (!context.apiKey || !context.apiSecret) {
+  //   throw new Error();
+  // }
+
+  const client = new Kucoin(context.apiKey, context.apiSecret);
+  // 5b374093cbdbf73e65d3f65d
+  // 58968302-6658-44a4-879a-e360b47f2187
+
+  const nonEmptyBalances = {};
+
+  await client.getBalance().then((res) => {
+    res.data.forEach(element => {
+      const BigNum = new BigNumber(element.balance);
+
+      if (BigNum.gt(new BigNumber(0))) {
+        nonEmptyBalances[element.coinType] = BigNum;
+      }
+    });
+  }).catch(console.error);
+
+  return nonEmptyBalances;
+};
+
+module.exports = {
+  getUserBalance
+};
+
+getUserBalance({apiKey: 'asdasd', apiSecret: 'asdas'});
