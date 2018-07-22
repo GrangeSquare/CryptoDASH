0cf55ee2f5b25070690310dbbde96dae3b50f775
JovanovicJovan <jjovanovic24@hotmail.com>
2018-07-22 12:54:14 +0200

    Added correct resources for front

--- services/post.js
+++ services/post.js
@@ -3,7 +3,11 @@
 const db = require('../models');
 
 async function getPostsFromDB () {
-  const posts = await db.Post.findAll({ limit: 5, order: [['created_at', 'DESC']] });
+  const posts = await db.Post.findAll({ 
+    limit: 5,
+    order: [['created_at', 'DESC']],
+    attributes: ['id_post', 'type', 'network_id']
+  });
 
   if (!posts) {
     throw new Error();
