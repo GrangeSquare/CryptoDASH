21e59697d8bb653913ebf56334588cb773d0f59d
kristianzivkovic357 <kristiano9611@gmail.com>
2018-08-28 14:10:47 +0200

    Small syntax fix.

--- social_network/listener.js
+++ social_network/listener.js
@@ -4,9 +4,8 @@ const listenerDB = require('./services/post');
 
 async function listenerEvents (req, res, next) {
   try {
-    // const data = JSON.parse(req.query.json);
-    console.log(req.query.json);
-    await listenerDB.putInDatabase(req.query.json);
+    console.log(req.body.json);
+    await listenerDB.putInDatabase(req.body.json);
     res.end();
   } catch (err) {
     console.log(err);
