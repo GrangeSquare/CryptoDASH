8ea62bb6cb402f5ed7e6d70871742b009c47e142
JovanovicJovan <jjovanovic24@hotmail.com>
2018-08-28 13:33:15 +0200

    Added real webhook

--- social_network/app.js
+++ social_network/app.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const listener = require('./listener');
 
-app.get('/listener', listener.listenerEvents);
+app.post('/listener', listener.listenerEvents);
 
 app.listen(8081, () => console.log('Listening on port 8081!'));
--- social_network/listener.js
+++ social_network/listener.js
@@ -4,8 +4,9 @@ const listenerDB = require('./services/post');
 
 async function listenerEvents (req, res, next) {
   try {
-    const data = JSON.parse(req.query.json);
-    await listenerDB.putInDatabase(data);
+    // const data = JSON.parse(req.query.json);
+    console.log(req.query.json);
+    await listenerDB.putInDatabase(req.query.json);
     res.end();
   } catch (err) {
     console.log(err);
