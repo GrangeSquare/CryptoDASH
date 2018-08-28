d8d5fdd11febe27c756418c6acd13dcfbdcb7d56
kristianzivkovic357 <kristiano9611@gmail.com>
2018-08-28 22:18:02 +0200

    Changed hashtag validations from query to param.

--- validators/hashtag.js
+++ validators/hashtag.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const { query } = require('express-validator/check');
+const { param } = require('express-validator/check');
 const { vk } = require('../resources');
 
 const checkHashtag = [
-  query('hashtag')
+  param('hashtag')
     .not().isEmpty().withMessage(vk('hashtag_req'))
     .isInt([ {min: 1} ]).withMessage(vk('num'))
 ];
 
 const checkDay = [
-  query('day')
+  param('day')
     .not().isEmpty().withMessage(vk('day_req'))
     .isInt([ {min: 1} ]).withMessage(vk('num'))
 ];
